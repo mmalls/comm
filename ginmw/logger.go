c961c2520cfc463ac07b9500d8d897f7371db61b
xtlfy <tengfei68@yeah.net>
2017-11-12 18:40:46 +0800

    modify logger mw format

--- ginmw/logger.go
+++ ginmw/logger.go
@@ -1,7 +1,6 @@
 package ginmw
 
 import (
-	"fmt"
 	"math"
 	"os"
 	"time"
@@ -26,31 +25,26 @@ func Logger(logger log.Logger) gin.HandlerFunc {
 
 		c.Next()
 
-		stop := time.Since(start)
-		latency := int(math.Ceil(float64(stop.Nanoseconds()) / 1000.0))
-		statusCode := c.Writer.Status()
-		clientIP := c.ClientIP()
-		clientUserAgent := c.Request.UserAgent()
-		referer := c.Request.Referer()
-		hostname, err := os.Hostname()
-		if err != nil {
-			hostname = "unknown"
-		}
-		dataLength := c.Writer.Size()
-		if dataLength < 0 {
-			dataLength = 0
-		}
+		if logger.DebugEnabled() {
+			stop := time.Since(start)
+			latency := int(math.Ceil(float64(stop.Nanoseconds()) / 1000.0))
+			statusCode := c.Writer.Status()
+			clientIP := c.ClientIP()
+			referer := c.Request.Referer()
+			hostname, err := os.Hostname()
+			if err != nil {
+				hostname = "unknown"
+			}
+			dataLength := c.Writer.Size()
+			if dataLength < 0 {
+				dataLength = 0
+			}
 
-		if len(c.Errors) > 0 {
-			logger.Error(c.Errors.ByType(gin.ErrorTypePrivate).String())
-		} else {
-			msg := fmt.Sprintf("%s - [%s] \"%s %s\" %d %d \"%s\" \"%s\" (%dms)", clientIP, hostname, c.Request.Method, path, statusCode, dataLength, referer, clientUserAgent, latency)
-			if statusCode > 499 {
-				logger.Error(msg)
-			} else if statusCode > 399 {
-				logger.Warn(msg)
+			if len(c.Errors) > 0 {
+				logger.Error(c.Errors.ByType(gin.ErrorTypePrivate).String())
 			} else {
-				logger.Info(msg)
+				logger.Debugf("[%s][%s][%s %s][%d][%d][%s][%s][%dms]",
+					clientIP, hostname, c.Request.Method, path, statusCode, dataLength, referer, latency)
 			}
 		}
 	}
